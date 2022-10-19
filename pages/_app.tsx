df327788ecda381c25a6509f863871aa401949ed
Duman Haydar <haydar@duman.io>
2022-10-19 21:20:18 +0200

    test

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,8 @@ import { SessionProvider } from "next-auth/react"
 import { Session } from "next-auth";
 import '../styles/index.scss'
 
+console.log(process.env)
+
 export default function MyApp({ Component, pageProps,
 }: AppProps<{
   session: Session;
