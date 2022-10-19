3fc4673a31810e075521741bad71c6f9982027da
Duman Haydar <haydar@duman.io>
2022-10-19 14:00:32 +0200

    correct image optimization

--- next.config.js
+++ next.config.js
@@ -5,6 +5,9 @@ const nextConfig = {
     sassOptions: {
         includePaths: [path.join(__dirname, "styles")],
     },
+    images: {
+        unoptimized: true
+    }
 };
 
 module.exports = nextConfig;
