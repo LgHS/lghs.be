6dced055e64379059ddc194b526e1e465fac6f2a
Duman Haydar <haydar@duman.io>
2022-10-19 21:53:47 +0200

    test

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -8,6 +8,14 @@ export const authOptions = {
             issuer: "https://auth.lghs.be/auth/realms/LGHS",
         })
     ],
-    debug: true
+    callbacks: {
+        async session({ session, token, user }) {
+          // Send properties to the client, like an access_token and user id from a provider.
+          session.accessToken = token.accessToken
+          session.user.id = token.id
+          
+          return session
+        },
+      }
 }
 export default NextAuth(authOptions)
