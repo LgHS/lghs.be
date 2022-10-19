03635d3569258b53c728d97327168d0bbb7864d2
Duman Haydar <haydar@duman.io>
2022-10-19 20:26:39 +0200

    update deploy test

--- pages/soutenir.tsx
+++ pages/soutenir.tsx
@@ -20,6 +20,7 @@ export default function Contact({ page, preview }: Props) {
   if (!router.isFallback && !page?.slug) {
     return <ErrorPage statusCode={404} />
   }
+  console.log(process.env.NEXTAUTH_URL)
   return (
     <Layout preview={preview}>
         {router.isFallback ? (
